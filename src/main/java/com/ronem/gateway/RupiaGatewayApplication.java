b7e5d66b42e957c7f3107f342a9aca61e73e5b34
Ram Mandal <ram.mandal@8squarei.com>
2026-01-16 09:56:24 +0545

    configure eureka discovery client

--- src/main/java/com/ronem/gateway/RupiaGatewayApplication.java
+++ src/main/java/com/ronem/gateway/RupiaGatewayApplication.java
@@ -2,8 +2,10 @@ package com.ronem.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RupiaGatewayApplication {
 
     public static void main(String[] args) {
