286b918695bb9db64d872920ebdbc560495bb3de
Ram Mandal <ram.mandal@8squarei.com>
2026-01-30 16:59:55 +0545

    update application.yaml, application-dev.yaml, application-docker.yaml, add security config file to create securityFilterchain bean

---
+++ src/main/java/com/ronem/gateway/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+/**
+ * Author: Ram Mandal
+ * Created on @System: Apple M1 Pro
+ * User:rammandal
+ * Date:30/01/2026
+ * Time:15:56
+ */
+
+
+package com.ronem.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
+                        //public endpoints
+                        .pathMatchers("/actuator/**").permitAll()
+//                        .pathMatchers("api/auth/**").permitAll()
+//                        .pathMatchers("/api/public/**").permitAll()
+//
+//                        //protected endpoints
+//                        .pathMatchers("/api/users/**").authenticated()
+//                        .pathMatchers("/api/payments/**").authenticated()
+//                        .pathMatchers("api/orders/**").authenticated()
+
+                        //All other requests
+                        .anyExchange().authenticated()
+                ).build();
+    }
+}
